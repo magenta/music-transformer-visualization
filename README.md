4b7e0ee579fd30ce4061a56e0d305f37f9a148da
Monica Dinculescu <noms@google.com>
2019-11-15 09:38:31 -0800

    add licenses

---
+++ README.md
@@ -0,0 +1,5 @@
+# Visualizing Music Transformer
+
+This is the application used to generate the figures in the
+[Visualizing Music Self-Attention](https://ai.google/research/pubs/pub48631) paper
+by Huang et al.
--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { Parser } from './parser.js';
 import { AppManager } from './app_manager.js';
 import { TYPES, svgNS, getLocationHash, drawMusicLine } from './utils.js';
 
 // Globals, globals for everyone.
-// TODO: fix this parsing
+// TODO: fix this parsing for the anonymous link.
 const KIND = window.location.pathname.substr(1).replace('.html','')
     .replace('nips-workshop-visualization/','')
     .replace('transformer-visualization/', '');
